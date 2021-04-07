a4df7de63bf4fe3765f5aa92e08ab05858ca233c
Wojciech Francuzik <wolass@gmail.com>
2021-04-07 09:27:03 +0200

    data platform preview

--- app.R
+++ app.R
@@ -1,14 +1,4 @@
-# This Shiny web application allows to integrate data into one single platform
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-
-pacman::p_load(shiny,shinyFiles)
+library(shiny)
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -23,7 +13,7 @@ ui <- fluidPage(
         sidebarPanel(
 
             # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
+            fileInput("file1", "Choose a CSV DATA File",
                       multiple = FALSE,
                       accept = c("text/csv",
                                  "text/comma-separated-values,text/plain",
@@ -31,7 +21,8 @@ ui <- fluidPage(
 
             # Horizontal line ----
             tags$hr(),
-
+            #Formatting
+            tags$p('Select correct formatting'),
             # Input: Checkbox if file has header ----
             checkboxInput("header", "Header", TRUE),
 
@@ -57,20 +48,37 @@ ui <- fluidPage(
                          choices = c(Head = "head",
                                      All = "all"),
                          selected = "head"),
-            actionButton("upload", "Upload"),
-            shinyFilesButton('files',
-                             label='File select',
-                             title='Please select a file',
-                             multiple=FALSE)
-
+            # Input: Select a meta-data-file ----
+            fileInput("file2", "Choose a CSV META-DATA File",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv"))
         ),
 
         # Main panel for displaying outputs ----
         mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Preview",
+                                 # Output: Data file ----
+                                 tableOutput("contents"),
+                                 tags$hr(),
+                                 tags$p('The meta data for the file above'),
+                                 tableOutput("meta")
+                        ),
+                        tabPanel("Instructions",
+                                 p("Please provide the data in a .csv file, and
+                                   folow tidy data practices. That means that:",
+                                   tags$ol(
+                                       tags$li("each relevant data from one set of experiments should be in one table"),
+                                       tags$li("each observation (usually a sample), should be in one row"),
+                                       tags$li("each variable (usually a measuring parameter from this sample) should be in a separate column")
+                                   ),
 
-            # Output: Data file ----
-            tableOutput("contents")
-
+                                   "Please also provide the correct sample name (as provided on the QR code) in the first column."),
+                                 p("Please provide the METADATA in a seperate .csv file, where each row contains the the name of the variable and a description"),
+                                 p())
+            )
         )
 
     )
@@ -78,8 +86,6 @@ ui <- fluidPage(
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-
-        shinyFileChoose(input, 'files', root=c(root='.'), filetypes=c('', 'txt'))
 
     output$contents <- renderTable({
 
@@ -112,19 +118,40 @@ server <- function(input, output) {
         }
 
     })
-    #Save file upon button click
-    observeEvent(input$upload, {
-        save_data_flatfile <- function(data) {
-            data <- t(data)
-            file_name <- paste0(paste(get_time_human(), digest(data,
-                                                               algo = "md5"), sep = "_"), ".csv")
-            write.csv(x = data, file = file.path(results_dir, file_name),
-                      row.names = FALSE, quote = TRUE)
+# Here we input the metadata into the app.
+    output$meta <- renderTable({
+
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
+
+        req(input$file2)
+
+        # when reading semicolon separated files,
+        # having a comma separator causes `read.csv` to error
+        tryCatch(
+            {
+                df <- read.csv(input$file2$datapath,
+                               header = input$header,
+                               sep = input$sep,
+                               quote = input$quote)
+            },
+            error = function(e) {
+                # return a safeError if a parsing error occurs
+                stop(safeError(e))
+            }
+        )
+
+        if(input$disp == "head") {
+            return(head(df))
         }
+        else {
+            return(df)
+        }
+
     })
+
 }
 
 # Create Shiny app ----
 shinyApp(ui, server)
-
-
